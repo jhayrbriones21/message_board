414cb9169bbc607e596c3a226134f6c62c0dd4e3
Jhayr Briones <fdc.jhayr@gmail.com>
2022-10-12 12:59:14 +0800

    udpate cron job

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const simpleGitPromise = require('simple-git/promise')();
 const spawn = require('child_process').spawn
 
 // You can adjust the backup frequency as you like, this case will run once a day
-cron.schedule('*/5 * * * *', () => {
+cron.schedule('*/1 * * * *', () => {
 // Use moment.js or any other way to dynamically generate file name
-  const fileName = `test_${moment().format('YYYY_MM_DD_h_mm_ss')}.sql`
+  const fileName = `testdb_${moment().format('YYYY_MM_DD_h_mm_ss')}.sql`
   const wstream = fs.createWriteStream(`/Applications/XAMPP/htdocs/test/${fileName}`)
   console.log('---------------------')
   console.log('Running Database Backup Cron Job')
@@ -28,7 +28,36 @@ cron.schedule('*/5 * * * *', () => {
 			   .push(['-u', 'origin', 'master'], () => {
 			   		console.log('push successfully');
 			   });
+    })
+    .on('error', (err) => {
+      console.log(err)
+    })
+});
 
+// You can adjust the backup frequency as you like, this case will run once a day
+cron.schedule('*/1 * * * *', () => {
+// Use moment.js or any other way to dynamically generate file name
+  const fileName = `test_${moment().format('YYYY_MM_DD_h_mm_ss')}.sql`
+  const wstream = fs.createWriteStream(`/Applications/XAMPP/htdocs/test/${fileName}`)
+  console.log('---------------------')
+  console.log('Running Database Backup Cron Job')
+  const mysqldump = spawn('/Applications/XAMPP/bin/mysqldump', [ '--opt', '--host=localhost', '-u', 'root', `--password=`, 'test' ])
+
+  mysqldump
+    .stdout
+    .pipe(wstream)
+    .on('finish', () => {
+      console.log('DB Backup Completed!')
+
+      shellJs.cd('/Applications/XAMPP/htdocs/test');
+
+      simpleGit.add(fileName)
+			   .commit(fileName)
+			   .push(['-u', 'origin', 'master'], (success) => {
+			   		console.log('push successfully');
+			   },(err) => {
+			   		console.log(err);
+			   });
     })
     .on('error', (err) => {
       console.log(err)
