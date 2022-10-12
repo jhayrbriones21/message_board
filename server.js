013f4d54d8fda0baa3a0aebb7a085a9c4ba19fa6
Jhayr Briones <fdc.jhayr@gmail.com>
2022-10-12 12:47:34 +0800

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const simpleGitPromise = require('simple-git/promise')();
 const spawn = require('child_process').spawn
 
 // You can adjust the backup frequency as you like, this case will run once a day
-cron.schedule('*/1 * * * *', () => {
+cron.schedule('*/5 * * * *', () => {
 // Use moment.js or any other way to dynamically generate file name
   const fileName = `test_${moment().format('YYYY_MM_DD_h_mm_ss')}.sql`
   const wstream = fs.createWriteStream(`/Applications/XAMPP/htdocs/test/${fileName}`)
@@ -22,37 +22,15 @@ cron.schedule('*/1 * * * *', () => {
       console.log('DB Backup Completed!')
 
       shellJs.cd('/Applications/XAMPP/htdocs/test');
-      const repo = 'message_board';
 
-		// Add all files for commit
-		simpleGitPromise.add(`${fileName}`)
-		    .then(
-		       (addSuccess) => {
-		          console.log(addSuccess);
-		          console.log('add succes');
-		       }, (failedAdd) => {
-		       	console.log(failedAdd);
-		          console.log('adding files failed');
-		});
-		// Commit files as Initial Commit
-		// simpleGitPromise.commit(`DB backup`)
-		//    .then(
-		//       (successCommit) => {
-		//         console.log(successCommit);
-		//      }, (failed) => {
-		//      	console.log(failed);
-		//         console.log('failed commmit');
-		// });
-		// // Finally push to online repository
-		// simpleGitPromise.push('origin','master')
-		//     .then((success) => {
-		//        console.log('repo successfully pushed');
-		//     },(failed)=> {
-		//        console.log('repo push failed');
-		// });
+      simpleGit.add(fileName)
+			   .commit(fileName)
+			   .push(['-u', 'origin', 'master'], () => {
+			   		console.log('push successfully');
+			   });
 
     })
     .on('error', (err) => {
       console.log(err)
     })
-})
+});
